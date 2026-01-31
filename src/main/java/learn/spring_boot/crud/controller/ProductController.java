35a3aa96a37de1b592da8e9c9976bb05753756e3
Galand Noah <galandnoah@gmail.com>
2026-01-31 21:28:02 +0100

    Delete and Put endpoints

--- src/main/java/learn/spring_boot/crud/controller/ProductController.java
+++ src/main/java/learn/spring_boot/crud/controller/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
@@ -23,10 +25,30 @@ public class ProductController {
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts()
+    {
+        List<Product> products = productService.readAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<Product> addProduct(@Valid @RequestBody CreateProductDTO productDTO) throws Exception
     {
         Product product = productService.create(productDTO);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@Valid @RequestBody CreateProductDTO dto, int id)
+    {
+        Product product = productService.update(dto, id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable int id)
+    {
+        productService.delete(id);
     }
 }
