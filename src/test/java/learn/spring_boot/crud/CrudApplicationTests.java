b57c5df9af2470c68cb844b45063ddbaefb5f4fd
Galand Noah <galandnoah@gmail.com>
2026-01-31 12:57:48 +0100

    Configuration de la source de donnees

---
+++ src/test/java/learn/spring_boot/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring_boot.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
