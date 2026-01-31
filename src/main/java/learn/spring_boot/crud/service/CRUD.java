21a44ddb5544af1457060e1d59881fd2b582ef02
Galand Noah <galandnoah@gmail.com>
2026-01-31 21:14:21 +0100

    Contrat CRUD

---
+++ src/main/java/learn/spring_boot/crud/service/CRUD.java
@@ -0,0 +1,44 @@
+package learn.spring_boot.crud.service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface CRUD<E,D> {
+    /**
+     * Delete a product
+     * @param id the product id
+     * */
+    void delete(int id) throws Exception;
+
+    /**
+     * Create a product
+     * @param dto the create product dto
+     * @return E product
+     * */
+    E create(D dto) throws Exception;
+
+
+    /**
+     * Select all the products in the database
+     * @return List<E> list of products
+     * */
+    List<E> readAll();
+
+
+    /**
+     * Select product by id
+     * @param id the product id
+     * @return E product
+     * */
+    E readById(int id) throws Exception;
+
+    /**
+     * update product
+     * @param dto
+     * @return E entity
+     * */
+    E update(D dto, String id);
+
+}
