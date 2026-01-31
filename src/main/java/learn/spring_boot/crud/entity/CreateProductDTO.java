300fd42c3679b616cf597918925a9a27c8afed57
Galand Noah <galandnoah@gmail.com>
2026-01-31 21:15:20 +0100

    DTO

---
+++ src/main/java/learn/spring_boot/crud/entity/CreateProductDTO.java
@@ -0,0 +1,29 @@
+package learn.spring_boot.crud.entity;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import learn.spring_boot.crud.validator.ValidPrice;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateProductDTO {
+
+    @NotBlank(message = "Le nom de la marque ne peut etre pas vide ou null")
+    private String brand;
+
+
+    @Positive(message = "La masse est invalide, veuillez entrer une valeur positive")
+    private double mass;
+
+    @ValidPrice(message = "Prix invalide")
+    private BigDecimal price;
+}
