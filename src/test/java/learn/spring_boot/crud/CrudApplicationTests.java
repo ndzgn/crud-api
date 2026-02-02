f8ca3851613196447e2fcaddaaf586aabc046865
Galand Noah <galandnoah@gmail.com>
2026-02-02 14:00:49 +0100

    Modification annulee

--- src/test/java/learn/spring_boot/crud/CrudApplicationTests.java
+++ src/test/java/learn/spring_boot/crud/CrudApplicationTests.java
@@ -2,6 +2,8 @@ package learn.spring_boot.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class CrudApplicationTests {
